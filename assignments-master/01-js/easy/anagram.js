fc9ed63275a9aef71f6bd851863a0547e1f65a38
xplicitDev <masoodforwork@gmail.com>
2025-06-15 00:47:04 +0530

    anagram done

--- assignments-master/01-js/easy/anagram.js
+++ assignments-master/01-js/easy/anagram.js
@@ -5,10 +5,37 @@
 */
 
 function isAnagram(str1, str2) {
-  for (i = 0; i <= str1.length; i++) {
-    if (str1[i]) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  // str2 in an array
+  const used = [];
+  for (i = 0; i < str2.length; i++) {
+    used[i] = false;
+  }
+
+  // matching str1 & str2
+  for (i = 0; i < str1.length; i++) {
+    let found = false;
+
+    for (j = 0; j < str2.length; j++) {
+      if (str1[i] === str2[j] && !used[j]) {
+        used[j] = true;
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      return false;
     }
   }
+
+  return true;
 }
 
 module.exports = isAnagram;
