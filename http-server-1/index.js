aeae44fb5e3a5107ae5b3acc64be96e79dd79dea
xplicitDev <masoodforwork@gmail.com>
2025-06-17 00:19:54 +0530

    basic express

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const port = 3000;
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
