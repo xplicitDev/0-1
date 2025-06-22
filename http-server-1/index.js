5e6033309794d02ed4b51dcd0ac3c99eef87cd10
xplicitDev <masoodforwork@gmail.com>
2025-06-22 23:36:14 +0530

    boiler plate code for express

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -19,3 +19,17 @@ app.get("/", function (req, res) {
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}`);
 });
+
+/////boiler plate code
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
