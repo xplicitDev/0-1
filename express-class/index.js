2f8d232df5cf31c02afc4feecb1a73038dc9ea40
xplicitDev <masoodforwork@gmail.com>
2025-06-30 00:12:01 +0530

    queries in http express

--- express-class/index.js
+++ express-class/index.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express();
 
+function sum(n) {
+  let ans = 0;
+  for (i = 1; i <= n; i++) {
+    ans = ans + i;
+  }
+  return ans;
+}
+
 app.get("/", function (req, res) {
-  res.send("hi there!");
+  const n = req.query.n;
+  const ans = sum(n);
+  res.send("Hello your answer is " + ans);
 });
 
 app.listen(3000);
