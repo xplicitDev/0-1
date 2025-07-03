2cd1c8294d8b94928ec7a1e011ffe3ce3c0f5811
xplicitDev <masoodforwork@gmail.com>
2025-07-03 23:40:21 +0530

    GET in our kidneyHospital

--- express-class/kidneyHospital.js
+++ express-class/kidneyHospital.js
@@ -14,8 +14,24 @@ const users = [
 
 app.get("/", function (req, res) {
   const masoodKidneys = users[0].kidneys;
-  console.log(masoodKidneys);
-  res.send("Kidney Health " + masoodKidneys);
+  // console.log(masoodKidneys);
+  const numberOfKidneys = masoodKidneys.length;
+
+  let numberOfHealthyKidneys = 0;
+
+  for (i = 0; i < masoodKidneys.length; i++) {
+    if (masoodKidneys[i].healthy) {
+      numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+    }
+  }
+
+  const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+
+  res.json({
+    numberOfKidneys,
+    numberOfHealthyKidneys,
+    numberOfUnhealthyKidneys,
+  });
 });
 
 app.listen(3000);
