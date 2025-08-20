b32b5e8b8a4f60702abf5f13224cb655780979df
xplicitDev <masoodforwork@gmail.com>
2025-08-20 21:00:25 +0530

    Add health checkup endpoint with input validation, concept of middlewares

---
+++ express-class/introMiddlewares.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/health-checkup", function (req, res) {
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const kidneyId = req.query.kidneyId;
+
+  if (username != "masood" || password != "pass") {
+    s;
+    res.status(400).json({ msg: "Somethings up with your inputs" });
+    return;
+  }
+
+  if (kidneyId != 1 && kidneyId != 2) {
+    res.status(400).json({ msg: "Somethings up with your inputs" });
+    return;
+  }
+
+  res.json({
+    msg: "Your Kidney is fine!",
+  });
+});
+
+app.listen(4000);
