15b54f484d93b532ddb48f3e51002f5969f507de
xplicitDev <masoodforwork@gmail.com>
2025-08-22 21:29:13 +0530

    intro Zod

--- express-class/introZod.js
+++ express-class/introZod.js
@@ -1,18 +1,16 @@
-const express = require("express");
 const zod = require("zod");
-const app = express();
 
-const schema = zod.array(zod.number());
-
-app.use(express.json());
-
-app.post("/health-checkup", function (req, res) {
-  const kidneys = req.body.kidneys;
-  const response = schema.safeParse(kidneys);
-
-  res.send({
-    response,
+function inputValidate(obj) {
+  const schema = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(8),
   });
-});
 
-app.listen(3000);
+  const response = schema.safeParse(obj);
+  console.log(response);
+}
+
+inputValidate({
+  email: "masood@gmail.com",
+  password: "12345678",
+});
