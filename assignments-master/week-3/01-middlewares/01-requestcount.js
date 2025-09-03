2f3102580ec31e2effcf79917f1f22455e722fae
xplicitDev <masoodforwork@gmail.com>
2025-09-03 22:29:43 +0530

    L

--- assignments-master/week-3/01-middlewares/01-requestcount.js
+++ assignments-master/week-3/01-middlewares/01-requestcount.js
@@ -7,6 +7,7 @@ let requestCount = 0;
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
+
 app.use(function (req, res, next) {
   requestCount = requestCount + 1;
   next();
