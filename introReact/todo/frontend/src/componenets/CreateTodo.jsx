1a1bcc8bde44e65f1df73a8a6639df0488ece454
xplicitDev <masoodforwork@gmail.com>
2025-10-02 21:24:45 +0530

    recovered L (successfully)

---
+++ introReact/todo/frontend/src/componenets/CreateTodo.jsx
@@ -0,0 +1,13 @@
+export function CreateTodo() {
+  return (
+    <div>
+      <input type="text" placeholder="Title"></input>
+      <br />
+      <br />
+      <input type="text" placeholder="Description"></input>
+      <br />
+      <br />
+      <button>Add a Todo</button>
+    </div>
+  );
+}
