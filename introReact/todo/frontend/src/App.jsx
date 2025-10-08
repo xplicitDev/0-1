7f3578fecafd6d0cb5633185728050e8c1cde6e7
xplicitDev <masoodforwork@gmail.com>
2025-10-08 15:16:22 +0530

    again rookie mistakes, fck

--- introReact/todo/frontend/src/App.jsx
+++ introReact/todo/frontend/src/App.jsx
@@ -6,10 +6,10 @@ import { Todos } from "./componenets/Todos";
 function App() {
   const [todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todos").then(async function (res) {
-    const json = await res.json();
-    setTodos(json.todos);
-  });
+  // fetch("http://localhost:3000/todos").then(async function (res) {
+  //   const json = await res.json();
+  //   setTodos(json.todos);
+  // });
 
   return (
     <div>
--- introReact/todo/frontend/src/componenets/CreateTodo.jsx
+++ introReact/todo/frontend/src/componenets/CreateTodo.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 export function CreateTodo() {
-  const { title, setTitle } = useState("");
-  const { description, setDescription } = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div>
       <input
@@ -22,21 +23,21 @@ export function CreateTodo() {
         placeholder="Description"
         onChange={function (e) {
           const value = e.target.value;
-          setDescription(value.title);
+          setDescription(e.target.value);
         }}
       ></input>
       <br />
       <br />
       <button
         onClick={() => {
-          fetch("http:/localhost:3000/todos", {
+          fetch("http://localhost:3000/todos", {
             method: "POST",
             body: JSON.stringify({
               title: title,
               description: description,
             }),
             headers: {
-              contentType: "application/json",
+              "Content-type": "application/json",
             },
           }).then(async function (res) {
             const json = await res.json();
