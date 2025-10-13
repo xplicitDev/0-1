5e0db236b73f51b4cff67606859796b5fa6f1b15
xplicitDev <masoodforwork@gmail.com>
2025-10-13 22:02:48 +0530

    understanding wrappers

--- introReact/week-6-experiments/src/App.jsx
+++ introReact/week-6-experiments/src/App.jsx
@@ -1,53 +1,19 @@
-import { useState } from "react";
-import "./App.css";
-
-let counter = 4;
-
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "go to gym",
-      description: "hurry to gym fcker",
-    },
-    {
-      id: 2,
-      title: "take shower",
-      description: "clean yourself fcker",
-    },
-    {
-      id: 3,
-      title: "have dinner",
-      description: "eat good protien bruhh",
-    },
-  ]);
-
-  function addTodo() {
-    setTodos([
-      ...todos,
-      {
-        id: counter++,
-        title: Math.random(),
-        description: Math.random(),
-      },
-    ]);
-  }
   return (
     <>
-      <button onClick={addTodo}>Add Todo</button>
-      {todos.map((todo) => (
-        <Todo key={todo.id} title={todo.title} description={todo.description} />
-      ))}
+      <CardWrapper innerComponent={TextComponenet} />
     </>
   );
 }
 
-function Todo({ title, description }) {
+function TextComponenet() {
   return (
     <>
-      <h1>{title}</h1>
-      <h4>{description}</h4>
+      <div>Hello!</div>
     </>
   );
+}
+function CardWrapper({}) {
+  return <></>;
 }
 export default App;
