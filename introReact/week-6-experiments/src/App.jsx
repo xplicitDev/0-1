f5247d63984c70d9d4e583d84f867c317d230359
xplicitDev <masoodforwork@gmail.com>
2025-10-15 22:01:40 +0530

    week ruined

--- introReact/week-6-experiments/src/App.jsx
+++ introReact/week-6-experiments/src/App.jsx
@@ -1,18 +1,28 @@
 function App() {
-  return (
-    <>
-      <CardWrapper>Hi There!</CardWrapper>
-      <CardWrapper>Hi There Again!</CardWrapper>
-      <CardWrapper>
-        <CardWrapper>Hi There Again & Again!!</CardWrapper>
-      </CardWrapper>
-    </>
-  );
+  return <></>;
 }
 
-function CardWrapper({ children }) {
-  return (
-    <div style={{ border: "3px solid black", padding: 20 }}>{children}</div>
-  );
+function CardWrapper({}) {
+  return;
 }
 export default App;
+
+// ---Wrapper---
+
+// function App() {
+//   return (
+//     <>
+//       <CardWrapper>Hi There!</CardWrapper>
+//       <CardWrapper>Hi There Again!</CardWrapper>
+//       <CardWrapper>
+//         <CardWrapper>Hi There Again & Again!!</CardWrapper>
+//       </CardWrapper>
+//     </>
+//   );
+// }
+
+// function CardWrapper({ children }) {
+//   return (
+//     <div style={{ border: "3px solid black", padding: 20 }}>{children}</div>
+//   );
+// }
