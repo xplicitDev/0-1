9bd66018e8d8fc809c8b617ef369e3ac9bbb9ea5
xplicitDev <masoodforwork@gmail.com>
2025-10-20 21:38:26 +0530

    similarly like page navigation

--- introReact/week-6.2/src/App.jsx
+++ introReact/week-6.2/src/App.jsx
@@ -3,28 +3,50 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [selectedId, setSelectedId] = useState(1);
+
+  return (
+    <>
+      <button
+        onClick={function () {
+          setSelectedId(1);
+        }}
+      >
+        1
+      </button>
+      <button
+        onClick={function () {
+          setSelectedId(2);
+        }}
+      >
+        2
+      </button>
+      <button
+        onClick={function () {
+          setSelectedId(3);
+        }}
+      >
+        3
+      </button>
+
+      <Todo id={selectedId} />
+    </>
+  );
+}
+
+function Todo({ id }) {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:8080/todos").then(function (response) {
       setTodos(response.data.todos);
     });
-  }, []);
+  }, [id]);
 
-  return (
-    <>
-      {todos.map((todo) => (
-        <Todo title={todo.title} description={todo.description} />
-      ))}
-    </>
-  );
-}
-
-function Todo({ title, description }) {
   return (
     <div>
-      <h1>{title}</h1>
-      <h4>{description}</h4>
+      <h1>{todos.title}</h1>
+      <h4>{todos.description}</h4>
     </div>
   );
 }
