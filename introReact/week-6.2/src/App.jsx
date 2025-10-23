8bb8898630012a2cd006399db456307988e4a2c8
xplicitDev <masoodforwork@gmail.com>
2025-10-23 21:19:00 +0530

    useMemo

--- introReact/week-6.2/src/App.jsx
+++ introReact/week-6.2/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [inputValue, setInputValue] = useState(1);
 
-  let count = 0;
-  for (let i = 1; i <= inputValue; i++) {
-    count = count + i;
-  }
+  let count = useMemo(() => {
+    console.log("memo called");
+    let finalCount = 0;
+    for (let i = 1; i <= inputValue; i++) {
+      finalCount = finalCount + i;
+    }
+    return finalCount;
+  }, [inputValue]);
 
   return (
     <div>
