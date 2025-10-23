a881605d6de96668ccd9f964f4bd72fe32d68ec2
xplicitDev <masoodforwork@gmail.com>
2025-10-23 21:56:14 +0530

    useCallback

--- introReact/week-6.2/src/App.jsx
+++ introReact/week-6.2/src/App.jsx
@@ -1,38 +1,34 @@
-import { useMemo, useState } from "react";
+import { memo, useCallback, useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [inputValue, setInputValue] = useState(1);
+  const [count, setCount] = useState(0);
 
-  let count = useMemo(() => {
-    console.log("memo called");
-    let finalCount = 0;
-    for (let i = 1; i <= inputValue; i++) {
-      finalCount = finalCount + i;
-    }
-    return finalCount;
-  }, [inputValue]);
+  const onClick = useCallback(() => {
+    console.log("child clicked");
+  }, []);
 
   return (
     <div>
-      <input
-        onChange={function (e) {
-          setInputValue(e.target.value);
-        }}
-        placeholder={"Find sum from 1 to n"}
-      ></input>
-      <br />
-      Sum from 1 to {inputValue} is {count}
-      <br />
+      <Child onClick={onClick} />
       <button
         onClick={() => {
-          setCounter(counter + 1);
+          setCount(count + 1);
         }}
       >
-        Counter ({counter})
+        Click me {count}
       </button>
     </div>
   );
 }
+
+const Child = memo(({ onClick }) => {
+  console.log("child render");
+
+  return (
+    <div>
+      <button onClick={onClick}>Button clicked</button>
+    </div>
+  );
+});
 
 export default App;
