aeb756930d422c8bba018d90e3dbfae990b4135d
xplicitDev <masoodforwork@gmail.com>
2025-11-10 14:54:31 +0530

    useRef

--- introReact/week-6.2/src/App.jsx
+++ introReact/week-6.2/src/App.jsx
@@ -1,40 +1,21 @@
-import { useEffect, useState, useMemo } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function App() {
-  const [exchange1Data, setExchange1Data] = useState({});
-  const [exchange2Data, setExchange2Data] = useState({});
-  const [bankData, setBankData] = useState({});
+  const divRef = useRef();
 
   useEffect(() => {
-    // Some operation to get the data
-    setExchange1Data({
-      returns: 100,
-    });
-  }, []);
-
-  useEffect(() => {
-    // Some operation to get the data
-    setExchange2Data({
-      returns: 100,
-    });
-  }, []);
-
-  useEffect(() => {
-    // Some operation to get the data
     setTimeout(() => {
-      setBankData({
-        income: 100,
-      });
+      divRef.current.innerHTML = "10";
     }, 5000);
   }, []);
 
-  const cryptoReturns = useMemo(() => {
-    return exchange1Data.returns + exchange2Data.returns;
-  }, [exchange1Data, exchange2Data]);
+  const incomeTax = 20000;
 
-  const incomeTax = (cryptoReturns + bankData.income) * 0.3;
-
-  return <div>hi there, your income tax returns are {incomeTax}</div>;
+  return (
+    <div>
+      hi there, your income tax returns are <div ref={divRef}>{incomeTax}</div>
+    </div>
+  );
 }
 
 export default App;
